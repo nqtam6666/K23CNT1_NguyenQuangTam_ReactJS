5575aa9a795570d90bb00d60b6fadcfc246576a1
nqtam6666 <nguyenquangtam179@gmail.com>
2025-02-11 09:29:19 +0700

    Day02_newProject

---
+++ Day02/hvt-lab02/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Infomation</h1>
+        <p>Họ và tên: Nguyễn Quang Tâm</p>
+        <p>Mã sinh viên:  2310900093</p>
+        <p>Ngày sinh: 26/06/2005</p>
+        <p>Điện thoại: 096113844*</p>
+        <p>Tên lớp: K23CNT1</p>
+        <a
+          className="App-link"
+          href="https://facebook.com"
+          target="_blank"
+        >
+          Facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
