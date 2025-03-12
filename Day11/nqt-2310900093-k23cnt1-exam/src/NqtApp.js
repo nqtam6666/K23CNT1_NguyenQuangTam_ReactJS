1af9353aecaa56bba27271c6d7795a34f5397683
Nguyễn Quang Tâm <nguyenquangtam179@gmail.com>
2025-03-12 09:31:41 +0700

    Kiểm tra giữa môn React

--- Day11/nqt-2310900093-k23cnt1-exam/src/NqtApp.js
+++ Day11/nqt-2310900093-k23cnt1-exam/src/NqtApp.js
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react';
 
 const Nqt_Accounts = [
   { nqt_id: "SV001", nqt_name: "Nguyễn Quang Tâm", nqt_email: "nguyenquangtam179@gmail.com", nqt_amount: 5000000 },
-  { nqt_id: "SV002", nqt_name: "Nguyễn Quang B", nqt_email: "tranthib@gmail.com", nqt_amount: 666666 },
-  { nqt_id: "SV003", nqt_name: "Nguyễn Quang C", nqt_email: "levanc@gmail.com", nqt_amount: 666666 }
+  { nqt_id: "SV002", nqt_name: "Nguyễn Quang B", nqt_email: "b@gmail.com", nqt_amount: 666666 },
+  { nqt_id: "SV003", nqt_name: "Nguyễn Quang C", nqt_email: "c@gmail.com", nqt_amount: 666666 }
 ];
 
 function NqtApp() {
